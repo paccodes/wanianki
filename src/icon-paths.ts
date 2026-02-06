44c3c3989c22b0db244f0af84e891419e2ce3a83
Pierre-Alain Castella <pierrealain.castella@gmail.com>
2026-02-06 17:26:55 +0100

    feat: Add ChevronDown icon.

--- src/icon-paths.ts
+++ src/icon-paths.ts
@@ -1,6 +1,9 @@
 export const bookIconPath =
   "M270-80q-45 0-77.5-30.5T160-186v-558q0-38 23.5-68t61.5-38l395-78v640l-379 76q-9 2-15 9.5t-6 16.5q0 11 9 18.5t21 7.5h450v-640h80v720H270Zm10-217 80-16v-478l-80 16v478Z";
 
+export const chevronDownIconPath =
+  "M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z";
+
 export const chevronLeftIconPath =
   "M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z";
 
